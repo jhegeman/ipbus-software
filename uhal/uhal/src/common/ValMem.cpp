46787fc1df8d77651920cc752d3978a0a5effb1e
Andy Rose <arose@cern.ch>
2019-10-31 08:40:55 +0000

    Move Valmem to a mempool model

--- uhal/uhal/src/common/ValMem.cpp
+++ uhal/uhal/src/common/ValMem.cpp
@@ -36,7 +36,6 @@
 #include "uhal/log/log.hpp"
 #include "uhal/utilities/bits.hpp"
 
-
 namespace uhal
 {
 
@@ -47,7 +46,6 @@ namespace uhal
 
 
   template< typename T >
-
   _ValWord_<T>::_ValWord_ ( const T& aValue , const bool& aValid , const uint32_t aMask ) :
     _ValHeader_ ( aValid ) ,
     value ( aValue ) ,
@@ -58,7 +56,6 @@ namespace uhal
 
 
   template< typename T >
-
   _ValVector_<T>::_ValVector_ ( const std::vector<T>& aValue , const bool& aValid ) :
     _ValHeader_ ( aValid ),
     value ( aValue )
@@ -68,9 +65,29 @@ namespace uhal
 
 
 
-  ValHeader::ValHeader() :
-    mMembers ( new _ValHeader_ ( false ) )
+  ValHeader::ValHeader()
   {
+    boost::lock_guard<boost::mutex> lLock ( mMutex );
+    if( mPool.empty() )
+    {
+      mMembers = boost::shared_ptr< _ValHeader_ >( new _ValHeader_ ( false ) );
+    } else {
+      mMembers = mPool.back();
+      mPool.pop_back();
+
+      mMembers->valid = false;
+    }
+  }
+
+
+  ValHeader::~ValHeader() 
+  {
+    if(mMembers.use_count() == 1)
+    {
+      // If we are the last user, then return to the pool
+      boost::lock_guard<boost::mutex> lLock ( mMutex );
+      mPool.push_back( mMembers );
+    }
   }
 
 
@@ -88,25 +105,56 @@ namespace uhal
 
 
   template< typename T >
-  ValWord< T >::ValWord ( const T& aValue , const uint32_t& aMask ) :
-    mMembers ( new _ValWord_<T> ( aValue , false , aMask ) )
+  ValWord< T >::ValWord ( const T& aValue , const uint32_t& aMask )
   {
+    boost::lock_guard<boost::mutex> lLock ( mMutex );
+    if( mPool.empty() )
+    {
+      mMembers = boost::shared_ptr< _ValWord_<T> >( new _ValWord_<T> ( aValue , false , aMask ) );
+    } else {
+      mMembers = mPool.back();
+      mPool.pop_back();
+
+      mMembers->valid = false;
+      mMembers->value = aValue;
+      mMembers->mask = aMask;
+    }
   }
 
 
   template< typename T >
   ValWord< T >::ValWord ( const ValWord<T>& aVal ) :
     mMembers ( aVal.mMembers )
-  {
-  }
+  {}
 
 
   template< typename T >
-  ValWord< T >::ValWord() :
-    mMembers ( new _ValWord_<T> ( T() , false , 0xFFFFFFFF ) )
+  ValWord< T >::ValWord()
   {
+    boost::lock_guard<boost::mutex> lLock ( mMutex );
+    if( mPool.empty() )
+    {
+      mMembers = boost::shared_ptr< _ValWord_<T> > ( new _ValWord_<T> (  T() , false , 0xFFFFFFFF ) );
+    } else {
+      mMembers = mPool.back();
+      mPool.pop_back();
+
+      mMembers->valid = false;
+      mMembers->value = T();
+      mMembers->mask = 0xFFFFFFFF;
+    }    
   }
 
+  template< typename T >
+  ValWord< T >::~ValWord()
+  {
+    if(mMembers.use_count() == 1)
+    {
+      // If we are the last user, then return to the pool
+      boost::lock_guard<boost::mutex> lLock ( mMutex );
+      mPool.push_back( mMembers );
+    }
+  }
 
   template< typename T >
   bool ValWord< T >::valid()
@@ -186,9 +234,19 @@ namespace uhal
 
 
   template< typename T >
-  ValVector< T >::ValVector ( const std::vector<T>& aValues ) :
-    mMembers ( new _ValVector_<T> ( aValues , false ) )
+  ValVector< T >::ValVector ( const std::vector<T>& aValues )
   {
+    boost::lock_guard<boost::mutex> lLock ( mMutex );
+    if( mPool.empty() )
+    {
+      mMembers = boost::shared_ptr< _ValVector_<T> > ( new _ValVector_<T> ( aValues , false ) );
+    } else {
+      mMembers = mPool.back();
+      mPool.pop_back();
+
+      mMembers->valid = false;
+      mMembers->value = aValues;
+    }  
   }
 
 
@@ -200,16 +258,47 @@ namespace uhal
 
 
   template< typename T >
-  ValVector< T >::ValVector ( const uint32_t& aSize )  :
-    mMembers ( new _ValVector_<T> ( std::vector<T> ( aSize , T() ) , false ) )
+  ValVector< T >::ValVector ( const uint32_t& aSize ) 
   {
+    boost::lock_guard<boost::mutex> lLock ( mMutex );
+    if( mPool.empty() )
+    {
+      mMembers = boost::shared_ptr< _ValVector_<T> > ( new _ValVector_<T> ( std::vector<T> ( aSize , T() ) , false ) );
+    } else {
+      mMembers = mPool.back();
+      mPool.pop_back();
+
+      mMembers->valid = false;
+      mMembers->value.resize( aSize );
+    }
   }
 
 
   template< typename T >
-  ValVector< T >::ValVector() :
-    mMembers ( new _ValVector_<T> ( std::vector<T>() , false ) )
+  ValVector< T >::ValVector()
   {
+    boost::lock_guard<boost::mutex> lLock ( mMutex );
+    if( mPool.empty() )
+    {
+      mMembers = boost::shared_ptr< _ValVector_<T> > ( new _ValVector_<T> ( std::vector<T> () , false ) );
+    } else {
+      mMembers = mPool.back();
+      mPool.pop_back();
+
+      mMembers->valid = false;
+      mMembers->value.clear();
+    }    
+  }
+
+  template< typename T >
+  ValVector< T >::~ValVector()
+  {
+    if(mMembers.use_count() == 1)
+    {
+      // If we are the last user, then return to the pool
+      boost::lock_guard<boost::mutex> lLock ( mMutex );
+      mPool.push_back( mMembers );
+    }    
   }
 
 
@@ -386,9 +475,27 @@ namespace uhal
   }
 
 
+
+  std::deque< boost::shared_ptr< _ValHeader_ > > ValHeader::mPool;
+
+  template< typename T >
+  std::deque< boost::shared_ptr< _ValWord_< T > > > ValWord< T >::mPool;
+
+  template< typename T >  
+  std::deque< boost::shared_ptr< _ValVector_< T > > > ValVector< T >::mPool;
+
+
+  boost::mutex ValHeader::mMutex;
+
+  template< typename T >
+  boost::mutex ValWord< T >::mMutex;
+
+  template< typename T >  
+  boost::mutex ValVector< T >::mMutex;
+
+
   template class ValWord< uint8_t >;
   template class ValWord< uint32_t >;
-
   template class ValVector< uint8_t >;
   template class ValVector< uint32_t >;
 
